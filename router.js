2ab06addcf1880b2e7f26bf5fdf877d4595eb0ce
PattyOlvera <paty.orion@outlook.com>
2022-06-10 15:17:29 -0500

    Favorites page added, Finished Project

--- router.js
+++ router.js
@@ -1,6 +1,7 @@
 // import Navigo from 'navigo';
 import Stories from './pages/stories.js'
 import Item from './pages/item.js';
+import Favorites from './pages/favorites.js';
 
 const router = new Navigo(null, true, '#');
 
@@ -15,7 +16,8 @@ export default class RouterHandler {
             {path: '/new', page: Stories},
             {path: '/ask', page: Stories},
             {path: '/show', page: Stories},
-            {path: '/item', page: Item}
+            {path: '/item', page: Item},
+            {path: '/favorites', page: Favorites}
         ];
 
         routes.forEach(({path, page}) => {
